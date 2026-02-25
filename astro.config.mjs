e1748bc8a555d2fcda79666eb03b3db0fd9575b0
MikeFrank99 <michael.francazzi@gmail.com>
2026-02-25 13:56:05 +0100

    fix: deploy settings for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://MikeFrank99.github.io',
+  base: '/Willi-Bevande',
   integrations: [svelte()]
 });
